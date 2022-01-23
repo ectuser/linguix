f91731823fce729685d37a87162c57d619150c37
ectuser <m.ectuser@gmail.com>
2022-01-24 04:40:32 +0700

    Last changes.

--- src/content/scroll-util.ts
+++ src/content/scroll-util.ts
@@ -1,8 +1,9 @@
 export function scrollUtil(event: PointerEvent) {
   const popupHeight = 270;
   const offset = 20;
-
-  const append = event.clientY > popupHeight + offset ? 'top' : 'bottom';
+  const middleOfPage = window.screen.height / 2;
+  
+  const append = event.clientY > middleOfPage - offset ? 'top' : 'bottom';
 
   const x = event.pageX;
   const y = append === 'top' ? event.pageY - offset : event.pageY + popupHeight + offset;
